f0df47348b607181bccd06a1b63e3587b4ad054c
True Ghiassi <true@ghiassitrio.co.uk>
2016-11-05 14:30:38 +0000

    I have added a licence and copyright notices.

--- impulse_response_experiment.py
+++ impulse_response_experiment.py
@@ -1,15 +1,34 @@
 """
-This program runs an experiment on a 3D printer and
-captures the impulse response of the nozzle.  This 
-file is structured as several small functions that 
-are used in the do_it_all function at the bottom of the
-file.
+Copyright 2016 True Ghiassi <true@ghiassitrio.co.uk>
+
+This file is part of TTea.   
+
+TTea is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+TTea is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with TTea.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 import serial
 import time
 import csv
 import matplotlib.pyplot as plt
+
+"""
+This program runs an experiment on a 3D printer and
+captures the impulse response of the nozzle.  This 
+file is structured as several small functions that 
+are used in the do_it_all function at the bottom of the
+file.
+"""
 
 
 def get_data_from_printer(ser):
--- test_impulse_response_experiment.py
+++ test_impulse_response_experiment.py
@@ -1,4 +1,24 @@
 """
+Copyright 2016 True Ghiassi <true@ghiassitrio.co.uk>
+
+This file is part of TTea.   
+
+TTea is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+TTea is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with TTea.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+
+"""
 A few simple tests for the functions in 
 impulse_response_experiment that are easy to test.
 When run, this script should print out True, True ... etc.
